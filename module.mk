5596e1c2a214135bd4ce1cb8ad2698a0d06aa78d
Martino Migliavacca <martino.migliavacca@gmail.com>
2016-04-07 14:25:55 +0200

    -std=gnu11

--- module.mk
+++ module.mk
@@ -7,7 +7,7 @@ MODULE_NAME ?= uDC
 
 # Compiler options here.
 ifeq ($(USE_OPT),)
-  USE_OPT = -O0 -ggdb -fomit-frame-pointer -falign-functions=16
+  USE_OPT = -O0 -ggdb -fomit-frame-pointer -falign-functions=16 -std=gnu11
 endif
 
 # C specific options here (added to USE_OPT).
